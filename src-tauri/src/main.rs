6d02af3f1f460c1a5eec0972b576630c3a84bd76
SanteauX <hugo.joncour.k2o@gmail.com>
2023-09-19 23:31:15 -0400

    Added some menus and shortcuts

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,20 @@
 /* ATTRIBUTES */
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_substystem = "windows"
+)]
 
 /* IMPORTS */
+
+
+
 extern crate base64;
 
-use std::env; 
 use std::fs::File;
 use std::io::{self, Read, Write};
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+
 
 // use tauri::{CustomMenuItem, Menu, MenuItem, Submenu}; // for later
 
@@ -17,40 +26,73 @@ fn get_base64(path: String) -> String {
     let mut file_data: Vec<u8> = Vec::new();
     file.read_to_end(&mut file_data).expect("Failed to read file");
     let encoded_file: String = base64::encode(&file_data);
-
-    // _create_test(&encoded_file);
-
     return encoded_file;
 }
 
-fn _create_test(base64_string: &String) -> io::Result<()> {
-    println!("creating test file");
-    let file_path: &str = "/Users/----/Desktop/GifViewer/test/test.gif"; // Replace with your absolute file path
 
-    let mut file: File = match File::create(file_path) {
-        Ok(file) => file,
-        Err(e) => {
-            eprintln!("Error creating file: {}", e);
-            return Err(e);
-        }
-    };
-    let decoded_file: Vec<u8> = base64::decode(base64_string).unwrap();
-    match file.write_all(&decoded_file) {
-        Ok(()) => {
-            println!("Data written successfully.");
-            Ok(())
-        }
-        Err(e) => {
-            eprintln!("Error writing to file: {}", e);
-            Err(e)
-        }
-    }
+/* MENU */
+
+fn create_app_menu() -> Menu {
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    /*let quit:       CustomMenuItem = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close:      CustomMenuItem = CustomMenuItem::new("close".to_string(), "Close");
+    let open:       CustomMenuItem = CustomMenuItem::new("open".to_string(), "Open...");
+
+    let submenu:    Submenu        = Submenu::new("File", Menu::new()
+                                        .add_item(quit)
+                                        .add_item(close)
+                                        .add_item(open));
+    let menu:       Menu           = Menu::new()
+                                            .add_native_item(MenuItem::Copy)
+                                            .add_item(CustomMenuItem::new("hide", "Hide"))
+                                            .add_submenu(submenu);
+    return menu;
+    */
+    let file_menu: Submenu = Submenu::new("File", Menu::new()
+                                .add_item(CustomMenuItem::new("new".to_string(), "New").accelerator("CmdOrCtrl+N"))
+                                .add_item(CustomMenuItem::new("open".to_string(), "Open").accelerator("CmdOrCtrl+O"))
+                                .add_item(CustomMenuItem::new("save".to_string(), "Save").accelerator("CmdOrCtrl+S")));
+    
+    let view_menu: Submenu = Submenu::new("View", Menu::new()
+                                .add_item(CustomMenuItem::new("previous".to_string(), "Previous").accelerator("CmdOrCtrl+ArrowLeft"))
+                                .add_item(CustomMenuItem::new("next".to_string(), "Next").accelerator("CmdOrCtrl+ArrowRight"))
+                                .add_item(CustomMenuItem::new("first".to_string(), "First").accelerator("CmdOrCtrl+ArrowUp"))
+                                .add_item(CustomMenuItem::new("last".to_string(), "Last").accelerator("CmdOrCtrl+ArrowDown"))
+
+                            );
+
+    let main_menu: Submenu = Submenu::new("App", Menu::new()
+                                .add_native_item(MenuItem::Quit));
+
+                        
+
+    let menu: Menu = Menu::new()
+                                .add_submenu(main_menu)
+                                .add_submenu(file_menu)
+                                .add_submenu(view_menu);
+
+    return menu;
+
 }
+
+
 
 /* MAIN */
 
 fn main() {
+
     tauri::Builder::default()
+        .menu(create_app_menu())
+        .on_menu_event(|event: tauri::WindowMenuEvent|
+        match event.menu_item_id() {
+            "new"       =>      println!("Placeholder for new"),
+            "open"      =>      println!("Open menu item clicked"),
+            "save"      =>      println!("Save selected item"),
+            "previous"  =>      println!("Previous item"),
+            "next"      =>      println!("Last selected item"),
+            _           =>      println!("Other")
+
+        })
         .invoke_handler(tauri::generate_handler![get_base64])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
